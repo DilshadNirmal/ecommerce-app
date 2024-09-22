2e11f91726bca7deb16c16f6d5a6424d1d1dbf3f
Dilshad Nirmal <dilshadgov2020@gmail.com>
2024-09-22 15:55:45 +0530

    orders api completed

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,8 +1,33 @@
-const Orders = () => {
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {backendUrl} from '../App'
+
+const Orders = ({token}) => {
+    const [orders, setOrders] = useState([])
+
+    const fetchAllOrders = async () => {
+        if (!token) {
+            return null
+        }
+
+        try {
+            const response = await axios.post(`${backendUrl}/api/order/list`, {}, {headers: {token}})
+            if (response.data.success) {
+                setOrders(response.data.orders);
+
+            }
+        } catch (error) {
+
+        }
+    }
+
+    useEffect(() => {
+fetchAllOrders()
+    }, [token])
 
     return (
         <div>
-            
+
         </div>
     )
 }
